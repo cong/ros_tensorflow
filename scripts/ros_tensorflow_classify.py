efa22a59737173567b1cbb999a914cbd10ea0bf6
cong <iwangcong@outlook.com>
2018-05-12 22:30:14 +0800

    first commit

---
+++ scripts/ros_tensorflow_classify.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
+import tensorflow as tf
+import classify_image
+import os
+
+import re
+
+
+class RosTensorFlow():
+    def __init__(self):
+        # classify_image.maybe_download_and_extract()
+        self._session = tf.Session()
+
+        self._cv_bridge = CvBridge()
+
+        self._sub = rospy.Subscriber('usb_cam/image_raw', Image, self.callback, queue_size=1)
+        self._pub = rospy.Publisher('/result_ripe', String, queue_size=1)
+        self.score_threshold = rospy.get_param('~score_threshold', 0.1)
+        self.use_top_k = rospy.get_param('~use_top_k', 5)
+
+    def load(self, label_lookup_path, uid_lookup_path):
+
+        if not tf.gfile.Exists(uid_lookup_path):
+          tf.logging.fatal('File does not exist %s', uid_lookup_path)
+        if not tf.gfile.Exists(label_lookup_path):
+          tf.logging.fatal('File does not exist %s', label_lookup_path)
+
+        # Loads mapping from string UID to human-readable string
+        proto_as_ascii_lines = tf.gfile.GFile(uid_lookup_path).readlines()
+        uid_to_human = {}
+        p = re.compile(r'[n\d]*[ \S,]*')
+        for line in proto_as_ascii_lines:
+            parsed_items = p.findall(line)
+            uid = parsed_items[0]
+            human_string = parsed_items[2]
+            uid_to_human[uid] = human_string
+
+        # Loads mapping from string UID to integer node ID.
+        node_id_to_uid = {}
+        proto_as_ascii = tf.gfile.GFile(label_lookup_path).readlines()
+        for line in proto_as_ascii:
+            if line.startswith('  target_class:'):
+                target_class = int(line.split(': ')[1])
+            if line.startswith('  target_class_string:'):
+                target_class_string = line.split(': ')[1]
+                node_id_to_uid[target_class] = target_class_string[1:-2]
+
+        # Loads the final mapping of integer node ID to human-readable string
+        node_id_to_name = {}
+        for key, val in node_id_to_uid.items():
+            if val not in uid_to_human:
+                tf.logging.fatal('Failed to locate: %s', val)
+            name = uid_to_human[val]
+            node_id_to_name[key] = name
+
+        return node_id_to_name
+    def callback(self, image_msg):
+        cv_image = self._cv_bridge.imgmsg_to_cv2(image_msg, "bgr8")
+        # copy from
+        # classify_image.py
+        image_data = cv2.imencode('.jpg', cv_image)[1].tostring()
+        # Creates graph from saved GraphDef.
+        softmax_tensor = self._session.graph.get_tensor_by_name('softmax:0')
+        predictions = self._session.run(
+            softmax_tensor, {'DecodeJpeg/contents:0': image_data})
+        predictions = np.squeeze(predictions)
+        # Creates node ID --> English string lookup.
+        # node_lookup = classify_image.NodeLookup()
+        
+        #-----
+        node_lookup = self.load(PATH_TO_LABELS, PATH_TO_UID)
+        #-----
+        top_k = predictions.argsort()[-self.use_top_k:][::-1]
+        for node_id in top_k:
+            #------
+            if node_id not in node_lookup:
+                human_string = ''
+            else:
+                human_string = node_lookup[node_id]
+
+            #------
+
+            score = predictions[node_id]
+            if score > self.score_threshold:
+                rospy.loginfo('%s (score = %.5f)' % (human_string, score))
+                self._pub.publish(human_string)
+
+    def main(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    # classify_image.setup_args()
+    
+    ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    PATH_TO_CKPT = ROOT_PATH + '/include/classifier/classify_image_graph_def.pb'
+    PATH_TO_LABELS = ROOT_PATH + '/include/classifier/imagenet_2012_challenge_label_map_proto.pbtxt'
+    PATH_TO_UID = ROOT_PATH + '/include/classifier/imagenet_synset_to_human_label_map.txt'
+
+    with tf.gfile.FastGFile(PATH_TO_CKPT, 'rb') as f:
+    	graph_def = tf.GraphDef()
+    	graph_def.ParseFromString(f.read())
+    	_ = tf.import_graph_def(graph_def, name='')    
+    
+    
+    rospy.init_node('ros_tensorflow_classify')
+    tensor = RosTensorFlow()
+    tensor.main()
---
+++ scripts/ros_tensorflow_detect.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import Image
+from std_msgs.msg import Int16
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import numpy as np
+import os
+import sys
+from multiprocessing import Queue, Pool
+import tensorflow as tf
+#from utils.app_utils import FPS
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+
+class RosTensorFlow():
+    def __init__(self):
+        self._cv_bridge = CvBridge()
+        self._sub = rospy.Subscriber('usb_cam/image_raw', Image, self.callback, queue_size=1, buff_size=2**24)
+        print('Image converter constructor ')
+        self._pub = rospy.Publisher("/result_ripe", Image, queue_size=0)
+        #self._pub = rospy.Publisher('result', Int16, queue_size=1)
+
+    def detect_objects(image_np, sess, detection_graph):
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+        # Each box represents a part of the image where a particular object was detected.
+        boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+        # Actual detection.
+        (boxes, scores, classes, num_detections) = sess.run(
+            [boxes, scores, classes, num_detections],
+            feed_dict={image_tensor: image_np_expanded})
+
+        # Visualization of the results of a detection.
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            image_np,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=8)
+        return image_np
+    
+    def callback(self, image_msg):
+            
+        cv_image = self._cv_bridge.imgmsg_to_cv2(image_msg, "bgr8")
+        
+        with detection_graph.as_default():
+            with tf.Session(graph=detection_graph) as sess:
+                image_np = cv_image
+                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                image_np_expanded = np.expand_dims(image_np, axis=0)
+                image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+                # Each box represents a part of the image where a particular object was detected.
+                boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+                # Each score represent how level of confidence for each of the objects.
+                # Score is shown on the result image, together with the class label.
+                scores = detection_graph.get_tensor_by_name('detection_scores:0')
+                classes = detection_graph.get_tensor_by_name('detection_classes:0')
+                num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+                # Actual detection.
+                (boxes, scores, classes, num_detections) = sess.run(
+                    [boxes, scores, classes, num_detections],
+                    feed_dict={image_tensor: image_np_expanded})
+                # Visualization of the results of a detection.
+                vis_util.visualize_boxes_and_labels_on_image_array(
+                    image_np,
+                    np.squeeze(boxes),
+                    np.squeeze(classes).astype(np.int32),
+                    np.squeeze(scores),
+                    category_index,
+                    use_normalized_coordinates=True,
+                    line_thickness=8)
+        
+        try:
+            self._pub.publish(self._cv_bridge.cv2_to_imgmsg(image_np, "bgr8"))
+        except CvBridgeError as e:
+            print(e)    
+    
+    
+    def main(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    rospy.init_node('ros_tensorflow_detect')
+
+    #ROOT_PATH = os.path.abspath(os.path.join(sys.path[0], os.pardir))
+    ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+    # Path to frozen detection graph. This is the actual model that is used for the object detection.
+    #MODEL_NAME = 'ssd_inception_v2_coco_11_06_2017'
+    PATH_TO_CKPT = ROOT_PATH + '/include/object_detector/frozen_inference_graph.pb'
+
+    # List of the strings that is used to add correct label for each box.
+    PATH_TO_LABELS = ROOT_PATH + '/include/object_detector/mscoco_label_map.pbtxt'
+
+    NUM_CLASSES = 1
+
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+
+    # Loading label map
+    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
+
+    tensor = RosTensorFlow()
+    tensor.main()
+
+
